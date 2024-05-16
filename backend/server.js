bf7cef812e561c991f6bd1de8238c00667996670
Hasan <programmermdhasan@gmail.com>
2024-05-16 08:36:31 +0600

    --

--- backend/server.js
+++ backend/server.js
@@ -3,24 +3,27 @@ import products from "./data/products.js";
 // import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
+connectDB(); // database connection function call
+
 const port = process.env.PORT || 5000;
-const app = express()
+const app = express();
 // app.use(cors())
 
 app.get("/", (req, res) => {
-    res.send("Api is running......")
-})
+  res.send("Api is running......");
+});
 
 app.get("/api/products", (req, res) => {
-    res.json(products)
-})
+  res.json(products);
+});
 
 app.get("/api/products/:id", (req, res) => {
-    const {id} = req.params;
-    const product = products.find(item => item?._id === +id);
-    res.json(product)
-})
+  const { id } = req.params;
+  const product = products.find((item) => item?._id === +id);
+  res.json(product);
+});
 
 app.listen(port, () => {
-    console.log(`server running on ${port}`)
-})
+  console.log(`server running on ${port}`);
+});
